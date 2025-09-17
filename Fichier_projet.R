1a591e790e426bff34e9d41fdab752b00dffb93c
ARVOISMatthis <arvois.matthis60@gmail.com>
2025-09-17 23:26:16 +0200

    ajout de la base et début des analyses

--- Fichier_projet.R
+++ Fichier_projet.R
@@ -7,5 +7,35 @@ model <- plm(inv ~ value + capital, data = Grunfeld, model = "pooling")
 summary(model)
 
 #Pour trouver la fichier de datagouv
+#import de la base de donnée
+
+
+#lien internet de la base : 
+#https://www.data.gouv.fr/datasets/production-delectricite-par-filiere-et-couts-de-production-au-pas-horaire/
+
+#Portail original de la source des données :
+#https://opendata-corse-outremer.edf.fr/explore/dataset/courbe-de-charge-de-la-production-delectricite-par-filiere/information/?disjunctive.territoire
+
+data <- read.csv("courbe-de-charge-de-la-production-delectricite-par-filiere.csv", sep = ";")
+View(data)
+
+
+dim(data)
+#On est sur une base de données de 263 040 données avec 14 variables
+
+
+types_des_variables <- str(data)
+types_des_variables
+
+
+length(unique(data$Date))
+#Il y a plus de 52 000 dates différentes -> compliquer pour faire des analyses mais vasy.
+
+
+#si on regarde seulement au nivrau de la corse:
+library(dplyr)
+data_corse <- data %>% 
+  filter(Territoire == "Corse")
+
 
 
