36f82fa402c75eb143e0ce1c362f3e2e3dc85c8b
ARVOISMatthis <arvois.matthis60@gmail.com>
2025-09-17 23:03:09 +0200

    ajout de grunfled + plm

--- Fichier_projet.R
+++ Fichier_projet.R
@@ -1,1 +1,11 @@
 #premier fichier pour projet
+
+#Cours
+library(plm)
+data("Grunfeld", package = "plm")
+model <- plm(inv ~ value + capital, data = Grunfeld, model = "pooling")
+summary(model)
+
+#Pour trouver la fichier de datagouv
+
+
