12226bfd42bb0a8bd600cd1fa5421b6084ca1b67
ARVOISMatthis <arvois.matthis60@gmail.com>
2025-09-18 00:47:01 +0200

    ajout de graphiques et mise en place du truc de plm pdata.frame()

--- Fichier_projet.R
+++ Fichier_projet.R
@@ -1,4 +1,7 @@
 #premier fichier pour projet
+library(lubridate)
+library(dplyr)
+library(ggplot2)
 
 #Cours
 library(plm)
@@ -32,10 +35,41 @@ length(unique(data$Date))
 #Il y a plus de 52 000 dates différentes -> compliquer pour faire des analyses mais vasy.
 
 
-#si on regarde seulement au nivrau de la corse:
-library(dplyr)
-data_corse <- data %>% 
-  filter(Territoire == "Corse")
+###Je vais essayer de nettoyer un peu la base parce que les dates sont mal foutue###
+
+#Déja il faut convertir les dates en bon format
+
+new_data <- data
+
+
+new_data$Date <- ymd_hms(new_data$Date, tz = "UTC")
+new_data$Date_semi <- as.Date(data$Date)
+
+#il faut changer les noms des colones.
+
+colnames(new_data) <- c("territoire","statut","date","production_totale","thermique",
+                    "bagasse_charbon","hydraulique","micro_hydraulique","solaire_photovoltaique",
+                    "eolien","bioenergies","géothermie","importations","cmp_euro_mwh","date_semi")
+
+new_data <- new_data %>% 
+  mutate(territoire = as.factor(territoire)) %>% 
+  arrange(territoire, date,date_semi)
+
+data_panel_1 <- pdata.frame(new_data, index = c("territoire","date"))
+data_panel_2 <- pdata.frame(new_data, index = c("territoire","date_semi"))
+
+
+View(data_panel)
+
+
+#Premier graphique sur la production totale en regardant tous les territoires:
+
+graph_corse_1 <- data_panel_1 %>% 
+  ggplot(aes(x = date, y=production_totale, color = territoire)) +
+  geom_point() +
+  theme_minimal()
+graph_corse_1
+
 
 
 
