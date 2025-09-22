f6d88e45ff381edb33e29ff2fa34eafb1f111101
ARVOISMatthis <arvois.matthis60@gmail.com>
2025-09-22 12:26:07 +0200

    ajout de truc + modif de base + ajout graph

--- Fichier_projet.R
+++ Fichier_projet.R
@@ -35,6 +35,15 @@ length(unique(data$Date))
 #Il y a plus de 52 000 dates différentes -> compliquer pour faire des analyses mais vasy.
 
 
+
+
+
+
+
+
+
+#############e Modification de la base
+
 ###Je vais essayer de nettoyer un peu la base parce que les dates sont mal foutue###
 
 #Déja il faut convertir les dates en bon format
@@ -54,6 +63,28 @@ colnames(new_data) <- c("territoire","statut","date","production_totale","thermi
 new_data <- new_data %>% 
   mutate(territoire = as.factor(territoire)) %>% 
   arrange(territoire, date,date_semi)
+
+new_data$cmp_euro_mwh <- as.numeric(new_data$cmp_euro_mwh)
+
+new_data[is.nan(as.matrix(new_data))] <- NA
+
+
+
+unique(is.nan(as.matrix(new_data)))
+#La on nous dis bien qu'il n'y a pas de NaN
+
+new_data
+
+
+
+
+
+
+
+
+
+#On commence les analysese
+
 
 data_panel_1 <- pdata.frame(new_data, index = c("territoire","date"))
 
@@ -104,8 +135,60 @@ data_re_guy_reg
 
 #On voit une baisse de la production de la production totale moyenne d'electricité  par jour pour la Réunion, c'est l'inverse pour la Gyuane
 #Il faut aller regarder les situation géopolitique et peut petre le prix par megawatth
+#On regarde alors l'évolution du pris pour premièrement la guyane:
 
   
 
+graph_guy_prod <- new_data   %>% 
+  filter(territoire %in% c('Guyane'),
+         date_semi >= as.POSIXct("2022-01-01", tz = "UTC")) %>% 
+  group_by(territoire,date_semi) %>% 
+  summarise(mean_prod = mean(production_totale, na.rm = T),.groups = "drop") %>% 
+  ggplot(aes(x = date_semi, y = mean_prod, color = territoire)) +
+  geom_point(alpha = 0.1) +
+  geom_smooth(method = "lm", se = FALSE, linewidth = 1) +
+  theme_minimal()
+graph_guy_prod
 
 
+#finalement on regarde plutôt les deux pays en mêmes temps
+
+graph_guy_price <- new_data   %>% 
+  filter(territoire %in% c('Guyane','Réunion'),
+         date_semi >= as.POSIXct("2022-01-01", tz = "UTC")) %>% 
+  group_by(territoire,date_semi) %>% 
+  select(territoire,date_semi,production_totale,cmp_euro_mwh) %>% 
+  summarise(mean_price = mean(cmp_euro_mwh, na.rm = T), .groups =  "drop")
+  
+graph_guy_price
+
+#On remplace les NaN par des 0 puis on retire les lignes sans les 0
+graph_guy_price$mean_price <- ifelse(is.nan(graph_guy_price$mean_price),0,graph_guy_price$mean_price )
+
+###### ICI ca ne marche pas car ca retire tout, on va faire un graphique plus large.
+
+
+
+
+#Pas intéressant parce qu'il n'y a pas les données des prix après 2020
+
+graph_large <- new_data %>% 
+  filter(territoire %in% c("Guyane","Corse"),
+         date <= as.POSIXct("2020-01-01", tz = "UTC")) %>% 
+  group_by(territoire,date_semi) %>%
+  summarise(mean = mean(cmp_euro_mwh, na.rm = T), .groups =  "drop") %>% 
+  ggplot(aes(x = date_semi, y = mean, color = territoire))  +
+  geom_point(alpha = 0.5) +
+  geom_line() +
+  theme_minimal()
+graph_large
+
+
+
+#On regarde la base apres 2022: 
+
+
+
+
+new_data_2022 <- new_data %>% 
+  filter(date_semi >= as.POSIXct("2022-01-01", tz = "UTC"))
